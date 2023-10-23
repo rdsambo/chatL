81e36282c89c6358b57afa2039ea654c3e408516
RDSambo <rdsambo@gmail.com>
2023-10-23 13:46:58 +0200

    auto login

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,21 @@
-import { onAuthStateChanged } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  getDoc,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth, db } from "../core/firebaseConfig";
 import LeftSide from "../components/LeftSide";
-import { doc, getDoc } from "firebase/firestore";
 import { User } from "../core/types";
 import RightSide from "../components/RightSide";
 
@@ -34,11 +46,80 @@ export default function Home() {
           });
         });
       } else {
-        navigate("/login", { replace: true });
+        // navigate("/login", { replace: true });
+        const email = Liferay.ThemeDisplay.getUserEmailAddress();
+        const password = Liferay.ThemeDisplay.getUserId();
+        signInWithUser(email, password);
       }
     });
     return unsub;
   }, [navigate]);
+  
+  const createUser = () => {
+    const email = Liferay.ThemeDisplay.getUserEmailAddress();
+    const password = Liferay.ThemeDisplay.getUserId();
+    const userName = Liferay.ThemeDisplay.getUserName();
+    const userNameS = userName.split(' ');
+    const fName = userNameS[0];
+    let lName = "";
+    if (userNameS[1]) {
+      lName = userName.replace(userNameS[0], '');
+    }
+    const birthday = ""; 
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((user) => {
+      const userData = {
+        fName,
+        lName,
+        birthday,
+        email,
+        uid: user.user.uid,
+        picture: "",
+        search: [
+          lName.toLowerCase(),
+          fName.toLowerCase(),
+          email.toLowerCase(),
+          fName.toLowerCase() + " " + lName.toLowerCase(),
+        ],
+      };
+      setDoc(doc(db, "users", user.user.uid), userData);
+      addDoc(collection(db, "chats"), {
+        createdAt: serverTimestamp(),
+        lastMessage: "",
+        updatedAt: serverTimestamp(),
+        userIds: [user.user.uid, "rmbJQucAbjQvll9pV341OB8hxnx2"],
+        id: "",
+      }).then((docRef) => {
+        updateDoc(doc(db, "chats", docRef.id), { id: docRef.id });
+        // addDoc(collection(db, "messages"), {
+        //   chatId: docRef.id,
+        //   message:
+        //     "Hello i'm Islem medjahdi, I'm a computer science student in Algeria - Algiers, If you have any questions, let me know and don't forget to check my portfolio : https://islem-medjahdi-portfolio.vercel.app/",
+        //   sender: "rmbJQucAbjQvll9pV341OB8hxnx2",
+        //   type: "text",
+        //   createdAt: serverTimestamp(),
+        // }).then((docRef) => {
+        //   updateDoc(doc(db, "messages", docRef.id), {
+        //     messageId: docRef.id,
+        //   });
+        // });
+        // updateDoc(doc(db, "chats", docRef.id), {
+        //   updatedAt: serverTimestamp(),
+        //   message:
+        //     "Hello i'm Islem medjahdi, I'm a computer science student in Algeria - Algiers, If you have any questions, let me know and don't forget to check my portfolio : https://islem-medjahdi-portfolio.vercel.app/",
+        // });
+      });
+    })
+    // .catch((e) => setError(e.code))
+    // .finally(() => setLoading(false))
+    ;
+  };
+  const signInWithUser = (email: string, password: string) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .catch((e) => createUser())
+      // .finally(() => setLoading(false))
+      ;
+  };
   return (
     <div className="grid overflow-auto grid-cols-7 font-jakarta h-screen">
       {(openChat || windowWidth > 768) && (
